505a7101d5047fe7167aaa04dd6d37598adbcb01
Philippe <philippe.silva@outlook.com>
2023-08-07 13:36:51 -0300

    feat(redis): add project

---
+++ src/RedisPubisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+
+namespace RedisPubisher
+{
+    public static class Program
+    {
+        private const string RedisConnectionString = "localhost:6379";
+        private static readonly ConnectionMultiplexer _connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+
+        public static void Main()
+        {
+            var pubsub = _connection.GetSubscriber();
+
+            var channel = new RedisChannel("test-channel", RedisChannel.PatternMode.Auto);
+
+            pubsub.PublishAsync(channel, "This is a test message!!", CommandFlags.FireAndForget);
+            Console.Write("Message Successfully sent to test-channel");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/RedisSubscriber/RedisSubscriber/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+namespace RedisPubisher
+{
+    public static class Program
+    {
+        private const string RedisConnectionString = "localhost:6379";
+        private readonly static ConnectionMultiplexer _connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+
+        public static void Main()
+        {
+            Console.WriteLine("Listening test-channel");
+            var pubsub = _connection.GetSubscriber();
+
+            var channel = new RedisChannel("test-channel", RedisChannel.PatternMode.Auto);
+            pubsub.Subscribe(channel, (channel, message) => Console.Write("Message received from test-channel : " + message));
+            Console.ReadLine();
+        }
+    }
+}
